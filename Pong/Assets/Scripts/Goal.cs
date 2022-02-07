793331325f8c220eb4254f12a6621a5a2130f6db
Joseph Arredondo <80712293+JosephArr12@users.noreply.github.com>
2022-02-06 18:30:03 -0800

    Pong Part1

---
+++ Pong/Assets/Scripts/Goal.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Goal : MonoBehaviour
+{
+    public bool isP1Goal;
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            if (isP1Goal)
+            {
+                Debug.Log("Player 2 (Right) Scored");
+
+                GameObject.Find("Ball").GetComponent<Ball>().scoredDirection = -1;
+
+                GameObject.Find("Ball").GetComponent<Ball>().Reset();
+
+                GameObject.Find("Manager").GetComponent<Manager>().P2Score ++;
+
+                GameObject.Find("Manager").GetComponent<Manager>().ScoreKeeper();
+
+            }
+            else
+            {
+                Debug.Log("Player 1 (Left) Scored");
+                GameObject.Find("Ball").GetComponent<Ball>().scoredDirection = 1;
+                GameObject.Find("Ball").GetComponent<Ball>().Reset();
+                GameObject.Find("Manager").GetComponent<Manager>().P1Score++;
+                GameObject.Find("Manager").GetComponent<Manager>().ScoreKeeper();
+            }
+        }
+    }
+
+}
